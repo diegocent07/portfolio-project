bafd26444791f1e3ac232828e3174a3b1829cc61
diegocent07 <fabiancent1.dc@gmail.com>
2019-02-03 15:51:31 -0300

    blogs in admin

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=40)
+    image = models.ImageField(upload_to='images/blogs')
+    pubdate = models.DateTimeField()
+    body = models.TextField(blank=True)
